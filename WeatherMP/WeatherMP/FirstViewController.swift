fb31a3488348762d2812ea29d14ee2ea5637878e
Martin Pihooja <martin.pihooja@gmail.com>
2018-05-07 21:19:43 +0300

    Add project to the Git
    Create app with 3 tabs

---
+++ WeatherMP/WeatherMP/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  WeatherMP
+//
+//  Created by Greete Jõgi on 07/05/2018.
+//  Copyright © 2018 Martin Pihooja. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ WeatherMP/WeatherMP/ThirdViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ThirdViewController.swift
+//  WeatherMP
+//
+//  Created by Greete Jõgi on 07/05/2018.
+//  Copyright © 2018 Martin Pihooja. All rights reserved.
+//
+
+import UIKit
+
+class ThirdViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
